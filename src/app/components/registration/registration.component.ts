f818a8a73e8a5084fa76b9a19b7814f843f60e17
Abel Mapharisa <abelm@wyzcrew.com>
2020-06-26 14:54:43 +0200

    Well... couldn't finish e'rything on time... that's how far I got

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { UserDTO } from 'src/app/dto/user-dto';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -10,19 +11,24 @@ import { UserDTO } from 'src/app/dto/user-dto';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: UserService) { }
 
   dto: UserDTO;
   submitted = false;
-  errors: string[];
+  errors: string[] = new Array();
 
   ngOnInit(): void {
     this.dto = new UserDTO();
   }
 
   onRegFormSubmit(): void {
-    console.log("Form data: "+ JSON.stringify(this.dto));
-    this.submitted = true;
+    let newUuid = '';
+    this.service.createNewUser(this.dto)
+      .subscribe(dto => {
+        newUuid = dto.uuid;
+        this.submitted = true;
+        //TODO: maybe redirect to view page
+      });
   }
 
 }
