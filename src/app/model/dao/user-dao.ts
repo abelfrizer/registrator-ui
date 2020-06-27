6459c3418f56bba87d2c46b3ac5d8dc78994e081
Abel Mapharisa <abelm@wyzcrew.com>
2020-06-27 11:49:27 +0200

    added methods to the UserDAO to interact with the user's RESTful services

--- src/app/model/dao/user-dao.ts
+++ src/app/model/dao/user-dao.ts
@@ -6,21 +6,45 @@ export class UserDAO {
 
     constructor(private http: HttpClient, private baseUrl: string) { }
 
+    public findByUuid(uuid: string): Observable<HttpResponse<UserDTO>> {
+        const serviceUrl = this.getServiceURL('/' + uuid);
+        return this.http.get<UserDTO>(serviceUrl, {
+            headers: this.getHeaders(),
+            observe: 'response'
+        });
+    }
+
+    public search(): Observable<HttpResponse<UserDTO[]>> {
+        const serviceUrl = this.getServiceURL('/');
+        return this.http.get<UserDTO[]>(serviceUrl, {
+            headers: this.getHeaders(),
+            observe: 'response'
+        });
+    }
+
+    public findAll(): Observable<HttpResponse<UserDTO[]>> {
+        const serviceUrl = this.getServiceURL('/');
+        return this.http.get<UserDTO[]>(serviceUrl, {
+            headers: this.getHeaders(),
+            observe: 'response'
+        });
+    }
+
     public create(dto: UserDTO): Observable<HttpResponse<UserDTO>> {
+        const serviceUrl = this.getServiceURL('/');
+
+        return this.http.post<UserDTO>(serviceUrl, dto, {
+            headers: this.getHeaders(),
+            observe: 'response'
+        });
+    }
+
+    private getHeaders(): HttpHeaders {
         const httpHeaders = new HttpHeaders({
             'Content-Type': 'application/json',
             'Cache-Control': 'no-cache'
         });
-        const options = {
-            headers: httpHeaders,
-            observe: 'response'
-        };
-        const serviceUrl = this.getServiceURL('/');
-
-        return this.http.post<UserDTO>(serviceUrl, dto, {
-            headers: httpHeaders,
-            observe: 'response'
-        });
+        return httpHeaders;
     }
 
     private getServiceURL(servicePath: string): string {
